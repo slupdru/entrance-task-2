3aa1e80f0ecf25dc35df38724bdc427dd82ed653
slupdru <VyacheslavDryuchin@yandex.ru>
2018-01-02 02:42:40 +0300

    2

---
+++ scripts/edit-meet.js
@@ -0,0 +1,16 @@
+deleteButton = document.getElementsByClassName('buttons-container_delete')[0];
+modal = document.getElementsByClassName('modal-delete')[0];
+closeButtons = document.getElementsByClassName('buttons-container_cancel');
+deleteMobButton = document.getElementsByClassName('delete_button')[0];
+function openDeleteModal(){
+    modal.style.display = 'flex';
+}
+
+function closeDeleteModal(){
+    modal.style.display = 'none';
+}
+
+closeButtons[0].addEventListener('click', closeDeleteModal);
+closeButtons[1].addEventListener('click', closeDeleteModal);
+deleteButton.addEventListener('click', openDeleteModal);
+deleteMobButton.addEventListener('click', openDeleteModal);
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 let calendars = document.getElementsByClassName('calendar');
+let floorLasthours = document.getElementsByClassName('floor_lasthour');
 let body = document.getElementsByTagName('body');
 let floorMobileTitles = document.getElementsByClassName('floor_mobile-title');
 let main = document.getElementsByTagName('main');
@@ -17,12 +18,19 @@ function nullStyleOfMeetings(){
         }
 }
 
-function coordsOfTable(event, type){
+function coordsOfTable(event){
    if ((event.parentNode.classList.contains('floor_table-hour_completed')) || (event.parentNode.classList.contains('floor_table-hour_nobutton')) ){
         let targetNumber = [-1, -1];
-        for (let i = 0; i < 2 ; i++){
+        for (let i = 0; i < floorLasthours.length; i++){
+            floorLasthours[i].style.background = '';
+        }
+        for (let i = 0; i < 9 ; i++){
             for (let j= 0; j<16; j++){
-                rows[i][j].childNodes[1].style.background = '';
+                for (let k = 0; k < rows[i][j].childNodes.length; k++){
+                    if  ((rows[i][j].childNodes[k].classList!==undefined)&&(rows[i][j].childNodes[k].classList.contains('dark'))){
+                        rows[i][j].childNodes[k].style.background = '';
+                    }
+                }
                 if (event.parentNode===rows[i][j]) {
                     targetNumber[0] = i;
                     targetNumber[1] = j;
@@ -34,9 +42,16 @@ function coordsOfTable(event, type){
     }
     else{
         nullStyleOfMeetings();
-        for (let i = 0; i < 2 ; i++){
+        for (let i = 0; i < floorLasthours.length; i++){
+            floorLasthours[i].style.background = '';
+        }
+        for (let i = 0; i < 9 ; i++){
             for (let j= 0; j<16; j++){
-                rows[i][j].childNodes[1].style.background = '';
+                for (let k = 0; k < rows[i][j].childNodes.length; k++){
+                    if  ((rows[i][j].childNodes[k].classList!==undefined)&&(rows[i][j].childNodes[k].classList.contains('dark'))){
+                        rows[i][j].childNodes[k].style.background = '';
+                    }
+                }
             }
         }
     }
@@ -77,43 +92,67 @@ function hoverCompletedCells(event){
     let target = event.target;
     let targetNumber = coordsOfTable(target); 
     if (targetNumber!== undefined){
-        lightCells(targetNumber,'rgba(0,0,0,0.12)', true);
-        switch (targetNumber[0]) {
-            case 0:
-                for (let j = 0; j < 16; j++){
-                rows[0][j].childNodes[1].style.background = 'rgba(0,0,0,0.12)';
-                }
-              break;
-            case 1:
-            if (targetNumber[1])
-              break;
-          }
+        lightCells(targetNumber,'rgba(0,0,0,0.12)', false);
+
     }
 }
 function lightCells(targetcel, color, click){
     switch (targetcel[0]) {
+
         case 0:
            if (click===true) {meetings[0].style.display = 'block';}
             for (let j = 0; j < 16; j++){
             rows[0][j].childNodes[1].style.background = color;
+            floorLasthours[0].style.background = color;
             }
-          break;
+        break;
+
         case 1:
-            if (targetcel[1]<=4){
+            if (targetcel[1]<=5){
                 if (click===true) {meetings[1].style.display = 'block';}
                 for (let j = 0; j < 5; j++){
                     rows[1][j].childNodes[1].style.background = color;
                     }
             }
-            if (targetcel[1]>=8){
+            if (targetcel[1]>=7){
+                floorLasthours[1].style.background = color;
                 if (click===true) {meetings[2].style.display = 'block';}
                 for (let j = 7; j < 16; j++){
                     rows[1][j].childNodes[1].style.background = color;
                     rows[1][j].childNodes[5].style.background = color;
                     }
             }
-          break;
-      }
+        break;
+
+        case 2:
+            if (targetcel[1]<=5){
+                if (click===true) {meetings[3].style.display = 'block';}
+                for (let j = 0; j < 5; j++){
+                    rows[2][j].childNodes[1].style.background = color;
+                    }
+                }
+            if (targetcel[1]>=7){
+                floorLasthours[2].style.background = color;
+                if (click===true) {meetings[4].style.display = 'block';}
+                for (let j = 6; j < 16; j++){
+                    for (let k = 0; k < rows[2][j].childNodes.length; k++){
+                        if  ((rows[2][j].childNodes[k].classList!==undefined)&&(rows[2][j].childNodes[k].classList.contains('table-hour_3-block'))||(rows[2][j].childNodes[k].classList!==undefined)&&(rows[2][j].childNodes[k].classList.contains('table-hour_1-block'))){
+                            rows[2][j].childNodes[k].style.background = color;
+                        }
+                    }
+                    }
+            }
+        break;
+
+        case 3:
+            floorLasthours[3].style.background = color;
+            if (click===true) {meetings[5].style.display = 'block';}
+                for (let j = 0; j < 16; j++){
+                rows[3][j].childNodes[1].style.background = color;
+                }
+        break;
+
+            }
 }
 function bodyClick(event){
     if ((event.target.id === 'timebar_date1')||(event.target.id === 'timebar_date2')){
@@ -135,6 +174,7 @@ table[0].onclick = function(event) {
     nullStyleOfMeetings();
     let targetNumber = coordsOfTable(target);
     if (targetNumber!== undefined){
+        table[0].removeEventListener('mouseover', hoverCompletedCells);
         lightCells(targetNumber,'#98A9B9', true);
     }
     else{
@@ -157,9 +197,20 @@ function scrollPage(){
         }
     }
 }
+
+let timeNow = document.getElementsByClassName('time-now');
+let width = document.body.clientWidth;
+if (width>850){
+    timeNow[0].style.left = `${18 + (width-850)/15}px`
+}
+
+let ModalNewMeet = document.getElementsByClassName('modal-new-meet');
+if (window.location.hash==='#created-meet'){
+    ModalNewMeet[0].style.display = 'flex';
+}
+
 table[0].addEventListener('mousedown', activeTitileDown);
 table[0].addEventListener('mouseover', lightTitile);
 table[0].addEventListener('mouseover', hoverCompletedCells);
 main[0].addEventListener('scroll', scrollPage);
 body[0].addEventListener('click', bodyClick)
-console.log(document.location.href);
